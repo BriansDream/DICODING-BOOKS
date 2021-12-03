327ee252a8b28ec914ff03612437757d535513e5
brians dream <destarock1@gmail.com>
2021-12-04 00:43:35 +0700

    finish implement all HTTP Request method

--- src/script/view/main.js
+++ src/script/view/main.js
@@ -7,6 +7,7 @@ function main () {
     const BooksAPI = "https://books-api.dicoding.dev";
 
     const getBook = () => {
+        // Fetch default method http request = GET
         fetch(`${BooksAPI}/list`)
         .then((response) => {
            return response.json();
@@ -25,16 +26,74 @@ function main () {
             });
     }
 
-    const insertBook = () => {
-
+   function insertBook(book) {
+     
+   
+        fetch(`${BooksAPI}/add`,  {
+            method: "POST",
+            headers : {
+                "Content-Type": "application/json",
+                "X-Auth-Token": "12345"
+            },
+            body: JSON.stringify(book),
+        
+        })
+        .then(response => {
+            return response.json();
+        })
+        .then(responseJson => { 
+            showResponseMessage(responseJson.message)
+            getBook();
+        }).catch(error => {
+            showResponseMessage(error);
+        })
+     
     }
 
-    const updateBook = () => {
+    const updateBook = (book) => {
 
+        const options = {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-Auth-Token': "12345"
+            },
+            body: JSON.stringify(book)
+        }
+
+        fetch(`${BooksAPI}/edit/${book.id}`, options)
+        .then((response) => {
+            return response.json();
+        })
+        .then(responseJson => {
+            showResponseMessage(responseJson.message);
+            getBook();
+        })
+        .catch(error => {
+            showResponseMessage(error);
+        })
+    
     }
 
-    const removeBook = () => {
-
+    const removeBook = (bookId) => {
+        fetch(`${BooksAPI}/delete/${bookId}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': "application/json",
+                "X-Auth-Token": "12345"
+            },
+            body: JSON.stringify(bookId)
+        })
+        .then(response => {
+            return response.json();
+        })
+        .then(responseJson => {
+            showResponseMessage(responseJson.message);
+            getBook();
+        })
+        .catch(error => {
+            showResponseMessage(error);
+        }) 
     }
 
   
@@ -50,8 +109,9 @@ function main () {
             const containerData = document.createElement('section');
             containerData.setAttribute('class','container-data');
             const idBook = document.createElement('div');
-            idBook.setAttribute('class','id-book');
+            idBook.setAttribute('class','container-idbook');
             const dataIdBook = document.createElement('p');
+            dataIdBook.setAttribute('class','id-book');
             dataIdBook.innerHTML = book.id;
             idBook.appendChild(dataIdBook);
             const titleBook = document.createElement('div');
@@ -67,34 +127,68 @@ function main () {
             
             const btnRemove = document.createElement('button');
             btnRemove.setAttribute('class','btnRemove');
-            btnRemove.innerHTML = "remove"
+            btnRemove.setAttribute('id',`${book.id}`)
+            btnRemove.innerHTML = "remove";
 
             containerData.appendChild(authorBook)
             containerData.insertBefore(titleBook,authorBook);
             containerData.insertBefore(idBook,titleBook);
-            containerData.appendChild(btnRemove);
-
+            containerData.appendChild(btnRemove);     
+                 
             containerDataResult.appendChild(containerData);
+       
+          
         });
-        }
 
+       
+        const removeButtons = document.querySelectorAll('.btnRemove');
+        
+        removeButtons.forEach((removeButton) => {
+                removeButton.addEventListener('click',(event) => {
+                    const bookId = event.target.id;
+                    removeBook(bookId);
+                })
+        })
+    }
+       
         const showResponseMessage = (message = "check your internet connection") => {
             alert(message);
         }
         
       
-        document.addEventListener('DOMContentLoaded', () => {
+        window.addEventListener('DOMContentLoaded', () => {
+            const idInput = document.querySelector('.book-id');
+            const titleInput = document.querySelector('.book-title');
+            const authorInput = document.querySelector('.book-author');
+            const btnSave = document.querySelector('.btn-save');
+         
+            const btnUpdate = document.querySelector('.btn-update');
+            
+            btnSave.addEventListener('click', function() {
+       
+                const book = {
+                    id: Number.parseInt(idInput.value),
+                    title: titleInput.value,
+                    author: authorInput.value,
+                }
+              
 
+                insertBook(book);
+            });
+
+            btnUpdate.addEventListener('click',(event) => {
+                const book = {
+                    id: Number.parseInt(idInput.value),
+                    title: titleInput.value,
+                    author: authorInput.value
+                }
+                updateBook(book);
+
+            })
 
 
             getBook();
         });
-        
-
-
-
-        getBook();
-
 
 }
 
